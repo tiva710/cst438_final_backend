d344b9e91531cbb0b943bd654e71f9c48f422ef7
Yong-github-10086 <yshu@csumb.edu>
2023-11-09 11:11:29 -0800

    yeah

---
+++ cst438_final_backend-dev/target/classes/data.sql
@@ -0,0 +1,4 @@
+insert into user_table
+(alias, email, password, role) values 
+('user', 'user@csumb.edu', '$2a$10$NVM0n8ElaRgg7zWO1CxUdei7vWoPg91Lz2aYavh9.f9q0e4bRadue','USER'),   
+('admin', 'admin@csumb.edu', '$2a$10$8cjz47bjbR4Mn8GMg9IZx.vyjhLXR/SKKMSZ9.mP9vpMu0ssKi8GW' , 'ADMIN');
